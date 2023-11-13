5251e8df562085562fcc883c96eaee7863f82721
unknown <Aman Singh@DESKTOP-ID4LD9C.fareast.corp.microsoft.com>
2023-11-13 18:55:37 +0530

    adding the source code for mangalyaan project

---
+++ src/App.js
@@ -0,0 +1,20 @@
+// import logo from './logo.svg';
+import './App.css'; 
+import './background.css';
+import './visulaizer/instructions'
+//import Navbar from './visulaizer/navbar';
+import Visualizer from './visulaizer/visualizer';
+
+function App() {
+  return (
+    <div className="App">  
+     <div id="stars" />
+     <div id="stars2" />
+      <div id="stars" />
+      
+      <Visualizer/> 
+    </div>
+  );
+}
+
+export default App;
